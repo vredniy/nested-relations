8d1f2367341b1c75fc6bae28b13eae5097ce61c1
Zudochkin Dmitry <dnd.pliz@gmail.com>
2012-08-18 18:36:44 +0400

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Qa::Application.config.secret_token = '46a134fec79ce39254e96a1ed8cd367a531a5dc164b3601f5812849e154643bf6780962bd656950fce8b9d61fe5dd588db373a2069ca383702a13ebc7355badf'
